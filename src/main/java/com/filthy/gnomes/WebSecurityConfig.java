702608a2c71d1c2337610f3fb5ee211e1e39d940
Tyzmo <lipan_andrei@yahoo.com>
2015-11-22 00:15:59 +0200

    titi

--- src/main/java/com/filthy/gnomes/WebSecurityConfig.java
+++ src/main/java/com/filthy/gnomes/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         @Override
         public void configure(WebSecurity web) throws Exception {
-            web.ignoring().antMatchers("/js/**").antMatchers("/images/**").antMatchers("/css/**");
+            web.ignoring().antMatchers("/js/**").antMatchers("/images/**").antMatchers("/css/**").antMatchers("/fonts/**");
         }
 
         @Override
