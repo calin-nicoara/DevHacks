ae28c3251fb188d4576d9cd3fd1af5647a0ceafe
Calin <calin2811@gmail.com>
2015-11-21 14:59:05 +0200

    Company and Room entities

---
+++ src/main/java/com/filthy/gnomes/entities/Company.java
@@ -0,0 +1,75 @@
+package com.filthy.gnomes.entities;
+
+import javax.persistence.*;
+
+/**
+ * Created by Calin on 11/21/2015.
+ */
+@Entity
+@Table(name = "company")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="COMPANY_SEQ_GEN")
+    @SequenceGenerator(name = "COMPANY_SEQ_GEN", sequenceName = "USER_ID_SEQ", allocationSize = 1)
+    long id;
+
+    String name;
+    String user;
+    String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Company company = (Company) o;
+
+        if (id != company.id) return false;
+        if (name != null ? !name.equals(company.name) : company.name != null) return false;
+        if (user != null ? !user.equals(company.user) : company.user != null) return false;
+        return !(password != null ? !password.equals(company.password) : company.password != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (user != null ? user.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/com/filthy/gnomes/entities/Room.java
@@ -0,0 +1,64 @@
+package com.filthy.gnomes.entities;
+
+import javax.persistence.*;
+
+/**
+ * Created by Calin on 11/21/2015.
+ */
+@Entity
+@Table(name = "room")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ROOM_SEQ_GEN")
+    @SequenceGenerator(name = "ROOM_SEQ_GEN", sequenceName = "ROOM_ID_SEQ", allocationSize = 1)
+    long id;
+
+    String name;
+    String details;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Room room = (Room) o;
+
+        if (id != room.id) return false;
+        if (name != null ? !name.equals(room.name) : room.name != null) return false;
+        return !(details != null ? !details.equals(room.details) : room.details != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (details != null ? details.hashCode() : 0);
+        return result;
+    }
+}
