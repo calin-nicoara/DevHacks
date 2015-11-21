3a3c0baaa4802874c858d21bd2a9bfaaf1f18bfa
AlexandruTanasescu <alexandru.vlad.tanasescu@gmail.com>
2015-11-21 15:01:44 +0200

    Meeting Entity

---
+++ src/main/java/com/filthy/gnomes/entities/Meeting.java
@@ -0,0 +1,35 @@
+package com.filthy.gnomes.entities;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "MEETING")
+public class Meeting {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEETING_SEQ_GEN")
+    @SequenceGenerator(name = "MEETING_SEQ_GEN", sequenceName = "MEETING_ID_SEQ", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "EMPLOYEE")
+    private String employee;
+
+    @Column(name = "CODE")
+    private String code;
+
+    @Column(name = "LOCATION")
+    private String location;
+
+    @Column(name = "TIME_END")
+    private LocalDateTime timeEnd;
+
+    @Column(name = "TIME_BEGIN")
+    private LocalDateTime timeBegin;
+
+    @OneToMany
+    @JoinColumn(name = "COMPANY_ID")
+    private String company;
+
+}
