6eb0d2cb659adda8b6899d929e5bbe796916b657
Calin <calin2811@gmail.com>
2015-11-21 17:56:24 +0200

    Made meeting dao restful

--- src/main/java/com/filthy/gnomes/dao/MeetingDAO.java
+++ src/main/java/com/filthy/gnomes/dao/MeetingDAO.java
@@ -2,12 +2,13 @@ package com.filthy.gnomes.dao;
 
 import com.filthy.gnomes.entities.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by Tyzmo on 11/21/2015.
  */
-@Repository
+@RepositoryRestResource(collectionResourceRel = "meetings", path = "meetings")
 public interface MeetingDAO extends JpaRepository<Meeting, Long>{
 
     Meeting findOneById(Long id);
