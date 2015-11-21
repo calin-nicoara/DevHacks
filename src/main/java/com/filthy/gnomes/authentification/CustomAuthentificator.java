caa457acb7a9bad7ea14ccdd57397d66a8dd341b
Tyzmo <lipan_andrei@yahoo.com>
2015-11-21 23:36:25 +0200

    titi

--- src/main/java/com/filthy/gnomes/authentification/CustomAuthentificator.java
+++ src/main/java/com/filthy/gnomes/authentification/CustomAuthentificator.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.List;
 /**
  * Created by Tyzmo on 11/21/2015.
  */
+@Component(value = "customAuthenticationProvider")
 public class CustomAuthentificator implements AuthenticationProvider {
 
     @Autowired
