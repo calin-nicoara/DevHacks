61d88a3df3f07800980235641d7d52827be3826a
unknown <calin2811@gmail.com@>
2015-11-21 10:15:54 +0200

    first commit

---
+++ src/main/java/com/filthy/gnomes/DevHacksApplication.java
@@ -0,0 +1,12 @@
+package com.filthy.gnomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevHacksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevHacksApplication.class, args);
+    }
+}
