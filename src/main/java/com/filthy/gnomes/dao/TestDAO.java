4d5c9837111fb2425b5e6ed468483613471d0188
Tyzmo <lipan_andrei@yahoo.com>
2015-11-21 15:04:05 +0200

    delete

--- src/main/java/com/filthy/gnomes/dao/TestDAO.java
+++
@@ -1,9 +0,0 @@
-package com.filthy.gnomes.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * Created by Tyzmo on 11/21/2015.
- */
-public interface TestDAO {
-}
--- src/main/java/com/filthy/gnomes/entities/TestEntity.java
+++
@@ -1,7 +0,0 @@
-package com.filthy.gnomes.entities;
-
-/**
- * Created by Tyzmo on 11/21/2015.
- */
-public class TestEntity {
-}
