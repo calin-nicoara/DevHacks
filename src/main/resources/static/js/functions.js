a86a17fb6c0b22d07f223c147e66a0391a80d782
Calin <calin2811@gmail.com>
2015-11-22 05:19:44 +0200

    Js in plus

---
+++ src/main/resources/static/js/functions.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Calin on 11/22/2015.
+ */
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+}
